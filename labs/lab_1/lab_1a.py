e8c43d4a7907714e4dbfb8fdaa06fb374daf55bd
breadloaf9 <lialyssa579@gmail.com>
2026-03-28 21:04:25 -0700

    Add intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Alyssa Li" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello! My name is Alyssa, and I'm a sophomore from California. A fun fact about me is that I like running, and am currently participating in my school's Track and Field team.")
 
 if __name__ == "__main__":
     main()
